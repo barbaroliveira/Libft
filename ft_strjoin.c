77dd4b00ae6b1a5b288bc9bb155069c14ae969f0
barbaroliveira <barbara.pocas@gmail.com>
2023-05-31 15:14:37 +0100

    changes in mandatory part 2

---
+++ ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bapereir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 15:06:43 by bapereir          #+#    #+#             */
+/*   Updated: 2023/05/31 15:06:59 by bapereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	int		i;
+	int		j;
+
+	dest = (char *) malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!dest)
+		return (0);
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	while (s1[i] != '\0')
+	{	
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		dest[i] = s2[j];
+		i++;
+		j++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
--- libft.h
+++ libft.h
@@ -59,5 +59,6 @@ char    *ft_strmapi(char const *s, char (*f) (unsigned int, char));
 char	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
